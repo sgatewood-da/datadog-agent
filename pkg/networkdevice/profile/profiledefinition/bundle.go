a6cb1abb318a1393d48c50f90bcd81c355134729
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-10-06 15:20:28 +0200

    Add source

--- pkg/networkdevice/profile/profiledefinition/bundle.go
+++ pkg/networkdevice/profile/profiledefinition/bundle.go
@@ -1,9 +1,21 @@
 package profiledefinition
 
+type ProfileSource string
+
+const (
+	SourceCustom  ProfileSource = "custom"
+	SourceDefault ProfileSource = "default"
+)
+
+// ProfileMetadata contains device profile metadata for downloaded profiles.
+type ProfileMetadata struct {
+	Source ProfileSource `json:"source"`
+}
+
 // ProfileEntry represent a profile entry with metadata.
 type ProfileEntry struct {
-	// Metadata can be added here later
-	Profile ProfileDefinition `json:"profile"`
+	Metadata ProfileMetadata   `json:"metadata"`
+	Profile  ProfileDefinition `json:"profile"`
 }
 
 // ProfileBundleResponse represent a list of profiles meant to be downloaded by user.
