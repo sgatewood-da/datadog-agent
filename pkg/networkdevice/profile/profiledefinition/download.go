4b4e866fd35300ce682d6100ec324c0956ed63c2
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-10-06 11:22:34 +0200

    refactor profiledefinition/download.go

--- pkg/networkdevice/profile/profiledefinition/download.go
+++ pkg/networkdevice/profile/profiledefinition/download.go
@@ -1,16 +1,8 @@
 package profiledefinition
 
-// DownloadProfileMetadata contains device profile metadata for downloaded profiles.
-type DownloadProfileMetadata struct {
-	CreatedAt int64  `json:"created_at"`
-	UpdatedAt int64  `json:"updated_at"`
-	Version   uint64 `json:"version"`
-}
-
 // DownloadProfileItem represent a profile.
 type DownloadProfileItem struct {
-	Profile  ProfileDefinition       `json:"profile,omitempty"`
-	Metadata DownloadProfileMetadata `json:"metadata,omitempty"`
+	Profile ProfileDefinition `json:"profile"`
 }
 
 // DownloadProfilesResponse represent a list of profiles meant to be downloaded by user.
