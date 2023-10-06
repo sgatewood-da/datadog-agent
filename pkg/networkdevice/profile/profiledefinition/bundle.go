864baf2bdf929f4748bcbe665956b7fba5500cf7
Alexandre Yang <alexandre.yang@datadoghq.com>
2023-10-06 15:24:05 +0200

    rename

--- pkg/networkdevice/profile/profiledefinition/bundle.go
+++ pkg/networkdevice/profile/profiledefinition/bundle.go
@@ -7,19 +7,19 @@ const (
 	SourceDefault ProfileSource = "default"
 )
 
-// ProfileMetadata contains device profile metadata for downloaded profiles.
-type ProfileMetadata struct {
+// ProfileBundleProfileMetadata contains device profile metadata for downloaded profiles.
+type ProfileBundleProfileMetadata struct {
 	Source ProfileSource `json:"source"`
 }
 
-// ProfileEntry represent a profile entry with metadata.
-type ProfileEntry struct {
-	Metadata ProfileMetadata   `json:"metadata"`
-	Profile  ProfileDefinition `json:"profile"`
+// ProfileBundleProfileItem represent a profile entry with metadata.
+type ProfileBundleProfileItem struct {
+	Metadata ProfileBundleProfileMetadata `json:"metadata"`
+	Profile  ProfileDefinition            `json:"profile"`
 }
 
 // ProfileBundleResponse represent a list of profiles meant to be downloaded by user.
 type ProfileBundleResponse struct {
-	Time     string         `json:"time"` // datetime when the bundle has been created
-	Profiles []ProfileEntry `json:"profiles"`
+	Time     string                     `json:"time"` // datetime when the bundle has been created
+	Profiles []ProfileBundleProfileItem `json:"profiles"`
 }
